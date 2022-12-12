28394dfcc8b0bd6fcb25ebda41cd170429c4bbde
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-12-12 23:10:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.devs = '60102810046';
 global.website = 'lynxaiman.my.id' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+  botname: process.env.BOT_NAME || 'lynxbot',
   ownername:process.env.OWNER_NAME || 'Lynxaiman',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
